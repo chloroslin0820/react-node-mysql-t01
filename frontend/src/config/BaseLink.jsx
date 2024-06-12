0477b5419e312a5bf0f633fbbb79137ad2c1d46b
chloros <chloros.lin@gmail.com>
2024-06-12 23:49:52 +0800

    change BASE_URL

--- frontend/src/config/BaseLink.jsx
+++ frontend/src/config/BaseLink.jsx
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:5000/books";
+// export const BASE_URL = "http://localhost:5000/books";
+export const BASE_URL = "https://react-node-mysql-t01.onrender.com/books";
